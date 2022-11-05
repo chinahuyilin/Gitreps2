dfaac5e7cf50b5bfcee93bb2952b6a10b3e1c622
hyl <640431734@qq.com>
2022-11-05 20:07:17 +0800

    第一次数据的提交

---
+++ src/com/hyl/Student.java
@@ -0,0 +1,12 @@
+package com.hyl;
+
+/**
+ * @Auther: hyl
+ * @Date: 2022/11/5 0005 - 11 - 05 - 20:04
+ * @Description: com.hyl
+ * @version: 1.0
+ */
+public class Student {
+    public  String name;
+    private  int age;
+}

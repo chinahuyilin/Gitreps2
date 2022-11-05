cdc3094f0b1664e4e54429f416760f03298468a0
hyl <640431734@qq.com>
2022-11-05 20:41:43 +0800

    在person里增加了age属性

---
+++ src/com/hyl/Person.java
@@ -0,0 +1,11 @@
+package com.hyl;
+
+/**
+ * @Auther: hyl
+ * @Date: 2022/11/5 0005 - 11 - 05 - 20:39
+ * @Description: com.hyl
+ * @version: 1.0
+ */
+public class Person {
+    private int age;
+}
